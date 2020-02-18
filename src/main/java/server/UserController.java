a44565908aae8d35f21dfbc20b5aceb209e15c58
Julian Lu <j.lu@student.fontys.nl>
2020-02-18 15:15:16 +0100

    Added login

--- src/main/java/server/UserController.java
+++ src/main/java/server/UserController.java
@@ -1,4 +1,61 @@
 package server;
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import server.classes.Order;
+import server.classes.User;
+import server.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/user/login")
+    public User login(@RequestBody Map<String, String> body){
+        String username = body.get("username");
+        String password = body.get("password");
+        User user = userRepository.findByUsernameAndPassword(username,password);
+
+        return user;
+    }
+
+    @PostMapping("/user/register")
+    public User register(@RequestBody Map<String, String> body){
+        String username = body.get("username");
+        String password = body.get("password");
+        User user = userRepository.findByUsernameAndPassword(username,password);
+
+        return user;
+    }
+
+    @GetMapping("/user/all")
+    public List<User> index(){
+        return userRepository.findAll();
+    }
+
+
+    public String hashPassword(String passwordPlaintext) {
+        int workload = 12;
+        String salt = BCrypt.gensalt(workload);
+        return BCrypt.hashpw(passwordPlaintext, salt);
+    }
+
+    public  boolean checkPassword(String password_plaintext, String storedHash) {
+        boolean passwordVerified = false;
+
+        if(null == storedHash || !storedHash.startsWith("$2a$"))
+            throw new IllegalArgumentException("Invalid hash provided for comparison");
+
+        passwordVerified = BCrypt.checkpw(password_plaintext, storedHash);
+
+        return(passwordVerified);
+    }
 }
--- src/main/java/server/classes/User.java
+++ src/main/java/server/classes/User.java
@@ -1,4 +1,34 @@
 package server.classes;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
 }
