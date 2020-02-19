451a44f47d8dda88631f97a3a2eee42df1f5fe5c
Julian Lu <j.lu@student.fontys.nl>
2020-02-19 14:22:32 +0100

    Register en Login met hash

--- koffieleutRest/src/main/java/server/repositories/OrderRepository.java
+++ koffieleutRest/src/main/java/server/repositories/OrderRepository.java
@@ -2,7 +2,7 @@ package server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import server.classes.Order;
+import server.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Integer> {
