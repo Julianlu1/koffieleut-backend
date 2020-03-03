19e27238461eae2387d2c970df88b481e384d2b5
Julian Lu <j.lu@student.fontys.nl>
2020-03-03 11:04:44 +0100

    Test

---
+++ src/test/java/server/CoffeeControllerTest.java
@@ -0,0 +1,30 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CoffeeController.class)
+@RunWith(SpringRunner.class)
+class CoffeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllCoffee() {
+
+        try {
+            mockMvc.perform(get("/coffee/all")).andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
