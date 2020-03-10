180f81ccd31a749db6b20170ecbb99eda524f6da
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2020-03-10 10:49:55 +0100

    controller map gemaakt

--- koffieleutRest/src/main/java/server/controller/CoffeeController.java
+++ koffieleutRest/src/main/java/server/controller/CoffeeController.java
@@ -1,4 +1,4 @@
-package server;
+package server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/server/ErrorMessage.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-public class ErrorMessage {
-    private String error;
-
-    public ErrorMessage(String error) {
-        this.error = error;
-    }
-}
